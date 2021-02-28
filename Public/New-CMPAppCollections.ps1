ce5a54e52a1926ac4f1040f449056c7ebd466110
Anthony J. Fontanez <ajf@anthonyfontanez.com>
2021-02-28 04:21:57 -0500

    Added checks for collection and folder existance, removed Set-Location calls, switched to generic New-CMCollection cmdlet

--- Public/New-CMPAppCollections.ps1
+++ Public/New-CMPAppCollections.ps1
@@ -24,19 +24,41 @@ function New-CMPAppCollections {
         [switch]$IsFreeApp
     )
     
-    Set-Location -Path "$($SiteCode):"
-    
     $UserCollectionName   = "$($AppName) - Available"
     $DeviceCollectionName = "$($AppName) - Required"
     $RefreshSchedule = New-CMSchedule -RecurInterval Hours -RecurCount 1
     
     if (-not $IsFreeApp) {
-        $UserCollection = New-CMUserCollection -Name $UserCollectionName -LimitingCollectionId $UserLimitingCollectionId -RefreshSchedule $RefreshSchedule -RefreshType Periodic
-        Move-CMObject -InputObject ($UserCollection) -FolderPath ".\UserCollection\Application"
+        if (-not (Get-CMCollection -CollectionType User -Name $UserCollectionName)) {
+            $UserCollection = New-CMCollection -CollectionType User -Name $UserCollectionName -LimitingCollectionId $UserLimitingCollectionId -RefreshSchedule $RefreshSchedule -RefreshType Periodic
+        }
+        else {
+            Write-Error -Message "User collection ""$UserCollectionName"" already exists"
+            break
+        }
+        
+        if (-not (Test-Path -Path ".\UserCollection\Application")) {
+            New-Item -Path ".\UserCollection\" -Name "Application" -ItemType Folder
+        }
+        
+        if ($null -ne $UserCollection) {
+            Move-CMObject -InputObject ($UserCollection) -FolderPath ".\UserCollection\Application"
+        }
     }
     
-    $DeviceCollection = New-CMDeviceCollection -Name $DeviceCollectionName -LimitingCollectionId $DeviceLimitingCollectionId -RefreshSchedule $RefreshSchedule -RefreshType Periodic
-    Move-CMObject -InputObject ($DeviceCollection) -FolderPath ".\DeviceCollection\Application"
+    if (-not (Get-CMCollection -CollectionType Device -Name $DeviceCollectionName)) {
+        $DeviceCollection = New-CMCollection -CollectionType Device -Name $DeviceCollectionName -LimitingCollectionId $DeviceLimitingCollectionId -RefreshSchedule $RefreshSchedule -RefreshType Periodic
+    }
+    else {
+        Write-Error -Message "Device collection ""$DeviceCollectionName"" already exists"
+        break
+    }
     
-    Set-Location -Path $env:SystemDrive
+    if (-not (Test-Path -Path ".\DeviceCollection\Application")) {
+        New-Item -Path ".\DeviceCollection\" -Name "Application" -ItemType Folder
+    }
+    
+    if ($null -ne $DeviceCollection) {
+        Move-CMObject -InputObject ($DeviceCollection) -FolderPath ".\DeviceCollection\Application"
+    }
 }
